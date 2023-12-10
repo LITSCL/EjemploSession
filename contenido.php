8fe5089bb76c4ceb151c4563fe11fd6d978892c7
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-09 22:51:09 -0300

    Ok

--- contenido.php
+++ contenido.php
@@ -1,11 +1,11 @@
 <?php
 session_start(); //Cada vez que se quiera trabajar con sesiones en un documento hay que utilizar "session_start()".
-if (empty($_SESSION["usuario"])) { //Se esta preguntando si la sesión existe.
+if (empty($_SESSION["usuario"])) { //Se esta preguntando si la sesiÃ³n existe.
     header("Location: login.php");
-    exit(); //Finaliza el Script (No se lee nada mas que este abajo de esta isntrucción).
+    exit(); //Finaliza el Script (No se lee nada mas que este abajo de esta isntrucciÃ³n).
 }
 
-//Aca van las instrucciones que se ejecutarán en caso de que la sesión si exista.
+//Aca van las instrucciones que se ejecutarÃ¡n en caso de que la sesiÃ³n si exista.
 echo "Bienvenido: " . $_SESSION["usuario"];
 echo "<a href='logout.php'>Cerrar Sesion</a>";
 ?>
